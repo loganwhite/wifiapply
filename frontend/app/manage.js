29ce0021b768d7f57b58644728a0609a796a169a
loganwhite <loganwhite@163.com>
2017-06-28 11:17:32 +0800

    add export excel for manager

--- frontend/app/manage.js
+++ frontend/app/manage.js
@@ -96,6 +96,30 @@ class ManageWindow extends React.Component {
 				<ListView background="#f1f2f4" width="1000" height="550">
 				<ListViewHeader>
 				<Text size="11" color="#696969">Order by sequence</Text>
+				<Button
+					onClick={function() {
+						$.fn.excelexportjs({
+							containerid: "dvjson"
+							, datatype: 'json'
+							, dataset: this.state.data
+							, columns: [
+								{ headertext: "No", datatype: "number", datafield: "id", ishidden: false }
+								, { headertext: "姓名", datatype: "string", datafield: "name", width: "100px" }
+								, { headertext: "学号", datatype: "string", datafield: "stuno", ishidden: false, width: "100px" }
+								, { headertext: "房间", datatype: "string", datafield: "room", ishidden: false }
+								, { headertext: "学院", datatype: "string",  datafield: "department", ishidden: false, width: "150px" }
+								, { headertext: "中心/教研室", datatype: "string",  datafield: "center", ishidden: false, width: "150px" }
+								, { headertext: "导师", datatype: "string",  datafield: "tuitor", ishidden: false, width: "150px" }
+								, { headertext: "邮箱", datatype: "string",  datafield: "email", ishidden: false, width: "150px" }
+								, { headertext: "手机", datatype: "string",  datafield: "mobile", ishidden: false, width: "150px" }
+								, { headertext: "是否发放", datatype: "number",  datafield: "isdistributed", ishidden: false, width: "150px" }
+							]
+						});
+
+					}.bind(this)}
+					marginTop="20px">
+					导出Excel
+				</Button>
 				</ListViewHeader>
 				<ListViewSection >
 					{this.renderItem('#','姓名','学号','房间','学院','中心/教研室','导师','邮箱','手机',true)}
@@ -207,3 +231,175 @@ class ManageWindow extends React.Component {
 
 ReactDOM.render(<ManageWindow />, document.getElementById('app'));
 
+(function ($) {
+    var $defaults = {
+        containerid: null
+        , datatype: 'table'
+        , dataset: null
+        , columns: null
+        , returnUri: false
+        , worksheetName: "My Worksheet"
+        , encoding: "utf-8"
+    };
+
+    var $settings = $defaults;
+
+    $.fn.excelexportjs = function (options) {
+        $settings = $.extend({}, $defaults, options);
+
+        var gridData = [];
+        var excelData;
+
+        return Initialize();
+
+        function Initialize() {
+            var type = $settings.datatype.toLowerCase();
+
+            BuildDataStructure(type);
+
+            switch (type) {
+                case 'table':
+                    excelData = Export(ConvertFromTable());
+                    break;
+                case 'json':
+                    excelData = Export(ConvertDataStructureToTable());
+                    break;
+                case 'xml':
+                    excelData = Export(ConvertDataStructureToTable());
+                    break;
+                case 'jqgrid':
+                    excelData = Export(ConvertDataStructureToTable());
+                    break;
+            }
+
+            if ($settings.returnUri) {
+                return excelData;
+            }
+            else {
+                window.open(excelData);
+            }
+        }
+
+        function BuildDataStructure(type) {
+            switch (type) {
+                case 'table':
+                    break;
+                case 'json':
+                    gridData = $settings.dataset;
+                    break;
+                case 'xml':
+                    $($settings.dataset).find("row").each(function (key, value) {
+                        var item = {};
+
+                        if (this.attributes != null && this.attributes.length > 0) {
+                            $(this.attributes).each(function () {
+                                item[this.name] = this.value;
+                            });
+
+                            gridData.push(item);
+                        }
+                    });
+                    break;
+                case 'jqgrid':
+                    $($settings.dataset).find("rows > row").each(function (key, value) {
+                        var item = {};
+
+                        if (this.children != null && this.children.length > 0) {
+                            $(this.children).each(function () {
+                                item[this.tagName] = $(this).text();
+                            });
+
+                            gridData.push(item);
+                        }
+                    });
+                    break;
+            }
+        }
+
+        function ConvertFromTable() {
+            var result = $('<div>').append($('#' + $settings.containerid).clone()).html();
+            return result;
+        }
+
+        function ConvertDataStructureToTable() {
+            var result = "<table>";
+
+            result += "<thead><tr>";
+            $($settings.columns).each(function (key, value) {
+                if (this.ishidden != true) {
+                    result += "<th";
+                    if (this.width != null) {
+                        result += " style='width: " + this.width + "'";
+                    }
+                    result += ">";
+                    result += this.headertext;
+                    result += "</th>";
+                }
+            });
+            result += "</tr></thead>";
+
+            result += "<tbody>";
+            $(gridData).each(function (key, value) {
+                result += "<tr>";
+                $($settings.columns).each(function (k, v) {
+                    if (value.hasOwnProperty(this.datafield)) {
+                        if (this.ishidden != true) {
+                            result += "<td";
+                            if (this.width != null) {
+                                result += " style='width: " + this.width + "'";
+                            }
+                            result += ">";
+                            result += value[this.datafield];
+                            result += "</td>";
+                        }
+                    }
+                });
+                result += "</tr>";
+            });
+            result += "</tbody>";
+
+            result += "</table>";
+            return result;
+        }
+
+        function Export(htmltable) {
+            var excelFile = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:x='urn:schemas-microsoft-com:office:excel' xmlns='http://www.w3.org/TR/REC-html40'>";
+            excelFile += "<head>";
+            excelFile += '<meta http-equiv="Content-type" content="text/html;charset=' + $defaults.encoding + '" />';
+            excelFile += "<!--[if gte mso 9]>";
+            excelFile += "<xml>";
+            excelFile += "<x:ExcelWorkbook>";
+            excelFile += "<x:ExcelWorksheets>";
+            excelFile += "<x:ExcelWorksheet>";
+            excelFile += "<x:Name>";
+            excelFile += "{worksheet}";
+            excelFile += "</x:Name>";
+            excelFile += "<x:WorksheetOptions>";
+            excelFile += "<x:DisplayGridlines/>";
+            excelFile += "</x:WorksheetOptions>";
+            excelFile += "</x:ExcelWorksheet>";
+            excelFile += "</x:ExcelWorksheets>";
+            excelFile += "</x:ExcelWorkbook>";
+            excelFile += "</xml>";
+            excelFile += "<![endif]-->";
+            excelFile += "</head>";
+            excelFile += "<body>";
+            excelFile += htmltable.replace(/"/g, '\'');
+            excelFile += "</body>";
+            excelFile += "</html>";
+
+            var uri = "data:application/vnd.ms-excel;base64,";
+            var ctx = { worksheet: $settings.worksheetName, table: htmltable };
+
+            return (uri + base64(format(excelFile, ctx)));
+        }
+
+        function base64(s) {
+            return window.btoa(unescape(encodeURIComponent(s)));
+        }
+
+        function format(s, c) {
+            return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; });
+        }
+    };
+})($);
